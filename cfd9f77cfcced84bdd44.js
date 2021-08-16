b2dc0226d93029b22f2e372d1993b3c07afb7bd8
Arthur Volokhin <artur.volokhin@mail.ru>
2021-08-16 22:36:00 +0300

    Updates

---
+++ cfd9f77cfcced84bdd44.js
@@ -0,0 +1,68 @@
+import "/sass/main.sass";
+import "/scripts/slider.js";
+import "/scripts/preset.js";
+import { fakeSearchData } from "./preset";
+
+const toggle = (name, classes) => {
+    document.querySelector(`${name}`).classList.toggle(`${classes}`);
+};
+
+window.onload = () => {
+    document.querySelector('.preloader').classList.add('hide');
+}
+
+const toggleMenu = () => {
+    toggle(".menu", "openMenu");
+    toggle(".blur", "block");
+};
+
+const toggleLogin = () => {
+    toggle(".login", "visible");
+    toggle(".blur", "block");
+};
+
+const setSearchWarning = (value) => {
+    document.querySelector('.search__mobile-warning').innerHTML = value;
+};
+
+document.addEventListener("click", ({ target }) => {
+    if (target.classList.contains("button__login")) {
+        toggleLogin();
+    }
+
+    if (target.closest(".burger")) {
+        toggleMenu();
+    }
+
+    if (target.closest(".header__search-mobile")) {
+        toggle(".search__mobile", "openSearch");
+    }
+
+    if (target.classList.contains("search__mobile-submit")) {
+        const searchField = document.querySelector(".search__mobile-field")
+        const value = searchField.value.toLowerCase();
+        const result = [];
+        //В базе данных получаем строки в виде тегов для поиска
+        //и сравниваем с полученным значением.
+        fakeSearchData.forEach((item) => {
+            const find = item.tagsName.toLowerCase().indexOf(value);
+            find !== -1 && result.push(item);
+        });
+        //Страхуемся от повторных значений и радуемся.
+        const foundResults = Array.from(new Set(result));
+
+        foundResults.length === 0 &&
+            setSearchWarning(`Nothing found at your request.
+            Try another value`);
+        
+        searchField.value = "";    
+    }
+});
+
+document.querySelector("#close__menu").addEventListener("click", () => {
+    toggleMenu();
+});
+
+document.querySelector("#close__login").addEventListener("click", () => {
+    toggleLogin();
+});
